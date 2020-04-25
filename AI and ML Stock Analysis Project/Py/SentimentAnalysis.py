209e4995190595402ce84c8ce32291b7f8747d9f
Harry G. Dulaney  IV <20808723+HarryDulaney@users.noreply.github.com>
2020-04-24 20:31:18 -0400

    Add files via upload

---
+++ AI and ML Stock Analysis Project/Py/SentimentAnalysis.py
@@ -0,0 +1,83 @@
+import time
+from urllib.request import urlopen
+import requests
+import pandas as pd
+import csv
+import numpy as np
+import keras as ker
+from bs4 import BeautifulSoup
+import nltk
+import warnings
+
+warnings.filterwarnings('ignore')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+analyzer = SentimentIntensityAnalyzer()
+
+# Begin update VADER's default Lexicon with Loughran and McDonald words lists and preconfigured stock lexicon
+# Read in the Loughran and McDonald words list for positive sentiment
+# For web deploy
+# url_pos = (
+#   'https://raw.githubusercontent.com/HarryDulaney/ai-machine-learning-finance/master/LoughranMcDonald_positive_word_lists.csv')
+
+# Read in the Loughran and McDonald words list for negative sentiment
+# url_neg = (
+#   'https://raw.githubusercontent.com/HarryDulaney/ai-machine-learning-finance/master/LoughranMcDonald_negative_word_lists.csv')
+
+
+positive = []
+with open('datasource/LoughranMcDonald_positive_word_lists.csv', 'r') as pos:
+    reader = csv.reader(pos)
+    for row in reader:
+        positive.append(row[0].strip())
+
+negative = []
+with open('datasource/LoughranMcDonald_negative_word_lists.csv', 'r') as neg:
+    reader = csv.reader(neg)
+    for row in reader:
+        entry = row[0].strip().split(" ")
+        if len(entry) > 1:
+            negative.extend(entry)
+        else:
+            negative.append(entry[0])
+
+custom_lexicon = {}
+custom_lexicon.update({word: 2.0 for word in positive})
+custom_lexicon.update({word: -2.0 for word in negative})
+#custom_lexicon.update(stock_lex_scaled)
+custom_lexicon.update(analyzer.lexicon)
+analyzer.lexicon = custom_lexicon
+
+# End VADER Lexicon customization
+
+scores = {}
+
+# Iterate through search results pages
+for i in range(1, 2):
+    page = urlopen('https://www.businesstimes.com.sg/search/microsoft?page=' + str(i)).read()
+    soup = BeautifulSoup(page, features="html.parser")
+    # Find the html tag matching <div class="media-body">
+    # This is the Headline, Paragraph, and Link for search results aka 'post'
+    posts = soup.findAll("div", {"class": "media-body"})
+    # Iterate over each post, open link to article, use Bsoup to find html paragraph tags, read and analyze the text
+    for post in posts:
+        time.sleep(1)
+        url = post.a['href']
+        date = post.time.text
+        print(date, url)
+        try:
+            link_page = urlopen(url).read()
+        except:
+            url = url[:-2]
+            link_page = urlopen(url).read()
+        link_soup = BeautifulSoup(link_page)
+        sentences = link_soup.findAll("p")
+        passage = ""
+        for sentence in sentences:
+            passage += sentence.text
+        sentiment = analyzer.polarity_scores(passage)['compound']
+        scores.setdefault(date, []).append(sentiment)
+
+print(scores)
